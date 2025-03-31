d96e5adf6f57aa8f50432acaf062a013d5f48aeb
Garrett Roberts <garrettmroberts@gmail.com>
2025-03-31 15:57:13 -0600

    Basic doubly linked list

--- Makefile
+++ Makefile
@@ -1,7 +1,8 @@
 all:
 	clang++ -std=c++17 -c LinkedList.cpp -o LinkedList.o
+	clang++ -std=c++17 -c DoublyLinkedList.cpp -o DoublyLinkedList.o
 	clang++ -std=c++17 -c main.cpp -o main.o
-	clang++ -std=c++17 main.o LinkedList.o -o output
+	clang++ -std=c++17 main.o LinkedList.o DoublyLinkedList.o -o output
 run: all
 	./output
 clean:
--- main.cpp
+++ main.cpp
@@ -1,26 +1,31 @@
 #include <iostream>
-#include "LinkedList.cpp"
+#include "DoublyLinkedList.cpp"
+
+using namespace std;
 
 int main()
 {
-  LinkedList list1;
+    DoublyLinkedList list;
+    
+    // Example usage of DoublyLinkedList methods
+    list.insertAtBeginning(30);
+    list.insertAtBeginning(20);
+    list.insertAtBeginning(10);
+    list.display(); // 10 20 30 null
+    cout << "Size of the list: " << to_string(list.size()) << endl; // Should print 3
+    list.insertAtEnd(40);
+    list.insertAtEnd(50);
+    list.display(); // 10 20 30 40 50 null
+    list.insertAtPosition(25, 2);
+    list.insertAtPosition(5, 0);
+    list.insertAtPosition(60, 10); 
+    list.display(); // 5 10 20 25 30 40 50 60 null
+    list.deleteFromBeginning();
+    list.display(); // 10 20 25 30 40 50 60 null
+    list.deleteFromEnd();
+    list.display(); // 10 20 25 30 40 50 null
+    list.deleteNode(25);
+    list.display(); // 10 20 30 40 50 null
 
-  list1.insertAtEnd(10);
-  list1.insertAtEnd(20);
-  list1.insertAtEnd(30);
-  list1.display(); // [10, 20, 30]
-
-  list1.insertAtBeginning(5);
-  list1.insertAtPosition(7, 1);
-  list1.display(); // [5, 7, 10, 20, 30]
-
-  list1.deleteFromBeginning();
-  list1.deleteFromEnd();
-  list1.display(); // [7, 10, 20]
-
-  list1.deleteFromPosition(1);
-  list1.display();                        // [7, 20]
-  std::cout << list1.size() << std::endl; // 2
-
-  return 0;
+    return 0;
 }
